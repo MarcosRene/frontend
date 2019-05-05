11cecbacf4c88708e466a1b62862540bd2e815b9
Marcos Rene <renemoura789@gmail.com>
2019-05-05 15:49:29 -0300

    Nova api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: "https://onmistack-backend.herokuapp.com"
+    baseURL: "https://backend-applications.herokuapp.com/"
 });
 
 export default api;
